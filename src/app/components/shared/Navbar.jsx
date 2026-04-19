a58e9445c4bf1d2492cb87f92c60b724c08f2597
alfu-git <mdalfaz.dev@gmail.com>
2026-04-19 19:37:04 +0600

    navbar done

--- src/app/components/shared/Navbar.jsx
+++ src/app/components/shared/Navbar.jsx
@@ -1,10 +1,109 @@
-import React from 'react';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const navLinks = (
+    <>
+      <li>
+        <Link
+          className={`${pathname === "/" ? "border-b-2 border-orange-600" : ""}`}
+          href="/"
+        >
+          Home
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          className={`${pathname === "/foods-cs" ? "border-b-2 border-orange-600" : ""}`}
+          href="/foods-cs"
+        >
+          Foods CS
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          className={`${pathname === "/foods-ss" ? "border-b-2 border-orange-600" : ""}`}
+          href="/foods-ss"
+        >
+          Foods SS
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          className={`${pathname === "/about" ? "border-b-2 border-orange-600" : ""}`}
+          href="/about"
+        >
+          About
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          className={`${pathname === "/contact" ? "border-b-2 border-orange-600" : ""}`}
+          href="/contact"
+        >
+          Contact
+        </Link>
+      </li>
+    </>
+  );
+
   return (
-    <div>
-      Navbar
-    </div>
+    <section className="container mx-auto px-5">
+      <nav>
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                {navLinks}
+              </ul>
+            </div>
+
+            <a className="text-orange-600 font-bold text-3xl">Foody</a>
+          </div>
+
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+          </div>
+
+          <div className="navbar-end">
+            <a className="btn border-none shadow-none bg-orange-600 text-base-100">Button</a>
+          </div>
+        </div>
+      </nav>
+    </section>
   );
 };
 
